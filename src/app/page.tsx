18e49202c6e161fb98d3baa1c141641312d8a900
Gabriel Nunes <gnmf2000@gmail.com>
2025-12-22 15:36:26 -0300

    chore: implement signout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { LogoutButton } from "@/features/logout/loggout-button";
 import { authClient } from "@/lib/auth-client";
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
@@ -6,12 +7,13 @@ import { caller } from "@/trpc/server";
 const Page = async () => {
   await requireAuth();
 
-  const users = await caller.getUsers()
+  const users = await caller.getUsers();
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
+    <div className="min-h-screen min-w-screen flex items-center justify-center gap-y-4 flex-col">
       Logado
       {JSON.stringify(users)}
+      <LogoutButton />
     </div>
   );
 };
