b5191556a65d054d672ba807497338b410412b77
Gabriel Nunes <gnmf2000@gmail.com>
2025-12-22 15:41:18 -0300

    fix: removing styles and imports from app folders

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,21 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
+import AuthLayout from "@/features/auth/components/auth-layout";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="bg-muted flex min-h-svh flex-col justify-center items-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link
-          href="/"
-          className="flex items-center gap-2 self-center font-medium"
-        >
-          <Image src="/logos/logo.svg" alt="logo" width={30} height={30} />
-          Node9n
-        </Link>
-        {children}
-      </div>
-    </div>
-  );
+  return <AuthLayout>{children}</AuthLayout>;
 };
 
 export default Layout;
