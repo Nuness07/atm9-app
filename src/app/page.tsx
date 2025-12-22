5009e1c504866f635f13522ad07e330d18a1b807
Gabriel Nunes <gnmf2000@gmail.com>
2025-12-22 14:44:24 -0300

    chore: implement better protected routes with trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
   await requireAuth();
 
+  const users = await caller.getUsers()
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       Logado
+      {JSON.stringify(users)}
     </div>
   );
 };
