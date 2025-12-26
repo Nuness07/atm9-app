72af0f675d38a19831960ac966246c63fbe47c9b
Gabriel Nunes <gnmf2000@gmail.com>
2025-12-26 13:53:06 -0300

    feat: implement workflow crud

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,39 +1,8 @@
-import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
-
-import { inngest } from "@/inngest/client";
-import prisma from "@/lib/db";
-import { createTRPCRouter, premiumProcedure, protectedProcedure } from "../init";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
-      },
-    });
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "gnmf2000@gmail.com",
-      },
-    });
-
-    return { success: true, message: "Workflow queued" };
-  }),
-  testAI: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-      data: {},
-    });
-
-    return { success: true, message: "AI Execution queued" };
-  }),
+  workflows: workflowsRouter,
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
