11e0346aa6ca2cd40948b773c7f345f95cd3ab42
Gabriel Nunes <gnmf2000@gmail.com>
2025-12-28 13:35:56 -0300

    fix: ensure getOne route returns workflow or throws error

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,12 +1,12 @@
 import { generateSlug } from "random-word-slugs";
+import z from "zod";
+import { PAGINATION } from "@/config/constants";
 import prisma from "@/lib/db";
 import {
   createTRPCRouter,
   premiumProcedure,
   protectedProcedure,
 } from "@/trpc/init";
-import z from "zod";
-import { PAGINATION } from "@/config/constants";
 
 export const workflowsRouter = createTRPCRouter({
   create: premiumProcedure.mutation(({ ctx }) => {
@@ -38,7 +38,7 @@ export const workflowsRouter = createTRPCRouter({
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(({ ctx, input }) => {
-      return prisma.workflow.findUnique({
+      return prisma.workflow.findUniqueOrThrow({
         where: { id: input.id, userId: ctx.auth.user.id },
       });
     }),
