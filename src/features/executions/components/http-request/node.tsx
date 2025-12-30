6d78f9d944c18b9a3611e7fae4f9d5d8d35adaee
Gabriel Nunes <gnmf2000@gmail.com>
2025-12-30 14:39:46 -0300

    fix: typos from nodeData

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -15,23 +15,21 @@ type HttpRequestNodeData = {
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
 
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
-  const nodeData = props.data as HttpRequestNodeData;
+  const nodeData = props.data;
   const description = nodeData?.endpoint
     ? `${nodeData.method || "GET"}: ${nodeData.endpoint}`
     : "Not configured";
 
   return (
-    <>
-      <BaseExecutionNode
-        {...props}
-        id={props.id}
-        icon={GlobeIcon}
-        name="HTTP Request"
-        description={description}
-        onSettings={() => {}}
-        onDoubleClick={() => {}}
-      />
-    </>
+    <BaseExecutionNode
+      {...props}
+      id={props.id}
+      icon={GlobeIcon}
+      name="HTTP Request"
+      description={description}
+      onSettings={() => {}}
+      onDoubleClick={() => {}}
+    />
   );
 });
 
