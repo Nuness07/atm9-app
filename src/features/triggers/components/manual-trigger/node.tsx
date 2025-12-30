879d5d0f9a7f0a5957c9f20858490fe19bd49f21
Gabriel Nunes <gnmf2000@gmail.com>
2025-12-30 14:29:33 -0300

    feat: implement new types of node - trigger and http request

---
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -0,0 +1,20 @@
+import type { NodeProps } from "@xyflow/react";
+import { MousePointerIcon } from "lucide-react";
+import { memo } from "react";
+import { BaseTriggerNode } from "../base-trigger-node";
+
+export const ManualTriggerNode = memo((props: NodeProps) => {
+  return (
+    <>
+      <BaseTriggerNode
+        {...props}
+        icon={MousePointerIcon}
+        name="When clicking 'Execute workflow'"
+        // onSettings={handleOpenSettings}
+        // onDoubleClick={handleOpenSettings}
+      />
+    </>
+  );
+});
+
+ManualTriggerNode.displayName = "HttpRequestNode";
